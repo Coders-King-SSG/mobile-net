91f3606cf648aacc15e1c64b015cfc900034dd9e
Coders-King-SSG <71335589+Coders-King-SSG@users.noreply.github.com>
2020-11-29 11:26:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 Webcam.set({
-    width:360,
-    height:310,
+    width:310,
+    height:300,
     image_format:'png',
     png_quality: 90,
     constraints : {
@@ -10,3 +10,32 @@ Webcam.set({
 var cam = document.getElementById('camera');
 
 Webcam.attach(cam);
+
+function snap() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById('result').innerHTML = '<img style="border-radius:15px;" src="'+data_uri+'" id="snapshot"/>'
+    });
+}
+
+var version = ml5.version;
+console.log('Version:', version);
+
+var classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+function modelLoaded() {
+    console.info('Model is loaded');
+}
+
+function predict() {
+    var img = document.getElementById('snapshot');
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById('object_name').innerHTML = results[0].label;
+    }
+}
